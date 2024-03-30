1910063943f3c85f82fd4d883f18d36e06b7d25f
chordzero <devchord0@gmail.com>
2024-03-30 21:21:53 +0900

    Preparing to use the page

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,10 @@
 import path from "path";
 import stylexPlugin from "@stylexjs/nextjs-plugin";
 
-const __dirname = import.meta.dirname
+const __dirname = import.meta.dirname;
 
 export default stylexPlugin({
+  output: "export",
   rootDir: __dirname,
   aliases: {
     "@/*": [path.join(__dirname, "*")],
